c774af960a3ad79134c7eb8199499635992c4375
nakajima-tasuku <nakajima-tasuku@agex.co.jp>
2025-07-30 14:50:59 +0900

    models and migration

---
+++ database/migrations/2025_07_29_151827_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->boolean('is_done');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign('tasks_user_id_foreign');
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
